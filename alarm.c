09e44b88fd9d1dad3540969090fb381d8355379b
bbalan <bbalan@visteon.com>
2020-07-23 13:52:12 +0530

    Comments Added.

--- alarm.c
+++ alarm.c
@@ -5,6 +5,7 @@
 
 void sig_handler(int signum)
 {
+
 	printf("Signal error number is %d\n",signum);
 	exit(0);
 }
@@ -13,9 +14,15 @@ void sig_handler(int signum)
 
 int main()
 {
+	/* Generate an Alarm signal after 5 seconds. */
 	alarm(5);
+	/* When an alarm signal SIGALRM was generated,
+	   handle this signal with a handler.
+	*/
 	signal(SIGALRM, sig_handler);
 
+	/* infinite loop, execution comes here only when
+           there is no exit in sig_handler. */
 	for(;;)
 		sleep(1);
 
